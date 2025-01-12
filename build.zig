d45856ccfa7729c87a8b48dc8a797c48998b630f
Theodore Sackos <theojsackos@hotmail.com>
2025-01-12 13:10:38 -0800

    Remove unnecessary

--- build.zig
+++ build.zig
@@ -24,8 +24,6 @@ pub fn build(b: *std.Build) void {
     const lib_unit_tests = b.addTest(.{
         .root_module = lib_mod,
     });
-    lib_unit_tests.root_module.addImport("c", luajit_build);
-
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
